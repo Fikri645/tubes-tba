57b0df6f22841d676615c9c4f1a9824f6e387240
Fikri Wahidin <fikriayam4@gmail.com>
2023-06-18 13:20:50 +0700

    parser dan lexer dibuat

--- tubes_lexer.py
+++ tubes_lexer.py
@@ -1,42 +1,39 @@
 import re
 
-# Token types
 TOKEN_FOR = 'FOR'
+TOKEN_PRINT = 'ACTION_PRINT'
+TOKEN_APLUSB = 'ACTION_APLUSB'
 TOKEN_SEMICOLON = 'SEMICOLON'
 TOKEN_BOOLEAN = 'BOOLEAN'
 TOKEN_COMPARE_OP = 'COMPARE_OP'
-TOKEN_ARITHMETIC_OP = 'ARITHMETIC_OP'
-TOKEN_INIT = 'INITIALIZE'
 TOKEN_ASSIGNMENT = 'ASSIGNMENT'
 TOKEN_INCREMENT = 'INCREMENT'
 TOKEN_DECREMENT = 'DECREMENT'
 TOKEN_OPEN_BRACE = 'OPEN_BRACE'
 TOKEN_CLOSE_BRACE = 'CLOSE_BRACE'
-TOKEN_IDENTIFIER = 'IDENTIFIER'
+TOKEN_VARIABLE = 'VARIABLE'
 TOKEN_DIGIT = 'DIGIT'
 TOKEN_WHITESPACE = 'WHITESPACE'
 TOKEN_UNKNOWN = 'UNKNOWN'
 
-# Regular expressions for token patterns
 TOKEN_PATTERNS = [
     (TOKEN_FOR, r'for'),
+    (TOKEN_PRINT, r'fmt.Print\(a\)'),
+    (TOKEN_APLUSB, r'a=a\+b'),
     (TOKEN_SEMICOLON, r';'),
     (TOKEN_BOOLEAN, r'(true|false)'),
     (TOKEN_COMPARE_OP, r'==|!=|>|<|>=|<='),
-    (TOKEN_ARITHMETIC_OP, r'\+|-|\*|/|%'),
-    (TOKEN_INIT, r':='),
     (TOKEN_ASSIGNMENT, r'='),
     (TOKEN_INCREMENT, r'\+\+'),
     (TOKEN_DECREMENT, r'--'),
     (TOKEN_OPEN_BRACE, r'\{'),
     (TOKEN_CLOSE_BRACE, r'\}'),
-    (TOKEN_IDENTIFIER, r'(a|b|c)'),
+    (TOKEN_VARIABLE, r'(a|b|c)'),
     (TOKEN_DIGIT, r'(0|1|2)'),
     (TOKEN_WHITESPACE, r'\s+'),
     (TOKEN_UNKNOWN, r'.')
 ]
 
-# Tokenize input source code
 def tokenize(source_code):
     tokens = []
     source_code = source_code.strip()
@@ -56,8 +53,7 @@ def tokenize(source_code):
             source_code = source_code[1:]
     return tokens
 
-# Example usage
-source_code = 'for a := 0; a < 2; i++ { c = a / b }'
+source_code = 'for a = 0; a < 2; i++ { a=a+b }'
 tokens = tokenize(source_code)
 for token_type, value in tokens:
     print(f'Token: {token_type}, Value: {value}')
--- tubes_parser.py
+++ tubes_parser.py
@@ -1,124 +1,280 @@
-import re
+def parser(sentence):
 
-# Token types
-TOKEN_FOR = 'FOR'
-TOKEN_SEMICOLON = 'SEMICOLON'
-TOKEN_ASSIGNMENT = 'ASSIGNMENT'
-TOKEN_IDENTIFIER = 'IDENTIFIER'
-TOKEN_COMPARE_OP = 'COMPARE_OP'
-TOKEN_ARITHMETIC_OP = 'ARITHMETIC_OP'
-TOKEN_INCREMENT = 'INCREMENT'
-TOKEN_DECREMENT = 'DECREMENT'
-TOKEN_OPEN_BRACE = 'OPEN_BRACE'
-TOKEN_CLOSE_BRACE = 'CLOSE_BRACE'
-TOKEN_BOOLEAN = 'BOOLEAN'
-TOKEN_DIGIT = 'DIGIT'
-TOKEN_STRING = 'STRING'
-TOKEN_WHITESPACE = 'WHITESPACE'
-TOKEN_UNKNOWN = 'UNKNOWN'
+    tokens = sentence.split()
+    tokens.append('EOS')
 
-# Grammar rules
-GRAMMAR = {
-    '<for_loop>': ['FOR', '<for_clause>', '<block>'],
-    '<for_clause>': ['<init>', 'SEMICOLON', '<condition>', 'SEMICOLON', '<inc_dec_stmt>'],
-    '<init>': ['<assignment>', 'SEMICOLON', '<condition>', 'SEMICOLON'],
-    '<condition>': ['<identifier>', '<comparison_op>', '<identifier>'],
-    '<inc_dec_stmt>': ['<identifier>', 'INCREMENT'],
-    '<block>': ['OPEN_BRACE', '<assignment>', 'CLOSE_BRACE'],
-    '<assignment>': ['<identifier>', 'ASSIGNMENT', '<expression>'],
-    '<expression>': ['<operand>', '<arithmetic_op>', '<operand>'],
-    '<arithmetic_op>': ['+', '-', '*', '/', '%'],
-    '<operand>': ['<literal>', '<identifier>'],
-    '<literal>': ['<boolean>', '<digit>', '<string>'],
-    '<identifier>': ['<letter>', '<letter>', '<digit>'],
-    '<letter>': ['a', 'b', 'c'],
-    '<string>': ['`', '<string_characters>', '`'],
-    '<string_characters>': ['<letter>', '<digit>', '<string_characters>', '<string_characters>'],
-    '<digit>': ['0', '1', '2'],
-    '<boolean>': ['true', 'false']
-}
+    non_terminals = ["S", "A", "B", "C", "D", "E", "F", "G"]
+    terminals = ["for", ";", "=", "++", "--", "{", "}", "==", "!=", ">", "<", ">=", "<=", 
+    "a", "b", "c", "0", "1", "2", "true", "false", "fmt.Print(a)", "a=a+b"]
 
+    parse_table = {}
 
-class Parser:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.current_token_index = 0
-        self.current_token = self.tokens[self.current_token_index]
+    parse_table[("S", "for")]           = ["for", "F", "A", "{", "G", "}"]
+    parse_table[("S", ";")]             = ["error"] 
+    parse_table[("S", "=")]             = ["error"] 
+    parse_table[("S", "++")]            = ["error"] 
+    parse_table[("S", "--")]            = ["error"]
+    parse_table[("S", "{")]             = ["error"] 
+    parse_table[("S", "}")]             = ["error"] 
+    parse_table[("S", "==")]            = ["error"] 
+    parse_table[("S", "!=")]            = ["error"]  
+    parse_table[("S", ">")]             = ["error"] 
+    parse_table[("S", "<")]             = ["error"] 
+    parse_table[("S", ">=")]            = ["error"] 
+    parse_table[("S", "<=")]            = ["error"] 
+    parse_table[("S", "a")]             = ["error"] 
+    parse_table[("S", "b")]             = ["error"] 
+    parse_table[("S", "c")]             = ["error"] 
+    parse_table[("S", "0")]             = ["error"] 
+    parse_table[("S", "1")]             = ["error"] 
+    parse_table[("S", "2")]             = ["error"] 
+    parse_table[("S", "true")]          = ["error"] 
+    parse_table[("S", "false")]         = ["error"] 
+    parse_table[("S", "fmt.Print(a)")]  = ["error"]
+    parse_table[("S", "a=a+b")]         = ["error"]
+    parse_table[("S", "EOS")]           = ["error"] 
 
-    def parse(self):
-        stack = ['<for_loop>']
-        while stack:
-            symbol = stack.pop()
-            if symbol in GRAMMAR:
-                production = GRAMMAR[symbol]
-                for item in reversed(production):
-                    stack.append(item)
+    parse_table[("A", "for")]           = ["error"]
+    parse_table[("A", ";")]             = ["error"] 
+    parse_table[("A", "=")]             = ["=", "E", ";", "B", ";", "F", "C"] 
+    parse_table[("A", "++")]            = ["error"] 
+    parse_table[("A", "--")]            = ["error"]
+    parse_table[("A", "{")]             = ["error"] 
+    parse_table[("A", "}")]             = ["error"] 
+    parse_table[("A", "==")]            = ["D", "E"] 
+    parse_table[("A", "!=")]            = ["D", "E"]  
+    parse_table[("A", ">")]             = ["D", "E"] 
+    parse_table[("A", "<")]             = ["D", "E"] 
+    parse_table[("A", ">=")]            = ["D", "E"] 
+    parse_table[("A", "<=")]            = ["D", "E"] 
+    parse_table[("A", "a")]             = ["error"] 
+    parse_table[("A", "b")]             = ["error"] 
+    parse_table[("A", "c")]             = ["error"] 
+    parse_table[("A", "0")]             = ["error"] 
+    parse_table[("A", "1")]             = ["error"] 
+    parse_table[("A", "2")]             = ["error"] 
+    parse_table[("A", "true")]          = ["error"] 
+    parse_table[("A", "false")]         = ["error"] 
+    parse_table[("A", "fmt.Print(a)")]  = ["error"]
+    parse_table[("A", "a=a+b")]         = ["error"]
+    parse_table[("A", "EOS")]           = ["error"] 
+
+    parse_table[("B", "for")]           = ["error"]
+    parse_table[("B", ";")]             = ["error"] 
+    parse_table[("B", "=")]             = ["error"] 
+    parse_table[("B", "++")]            = ["error"] 
+    parse_table[("B", "--")]            = ["error"]
+    parse_table[("B", "{")]             = ["error"] 
+    parse_table[("B", "}")]             = ["error"] 
+    parse_table[("B", "==")]            = ["error"] 
+    parse_table[("B", "!=")]            = ["error"]  
+    parse_table[("B", ">")]             = ["error"] 
+    parse_table[("B", "<")]             = ["error"] 
+    parse_table[("B", ">=")]            = ["error"] 
+    parse_table[("B", "<=")]            = ["error"] 
+    parse_table[("B", "a")]             = ["F", "D", "E"] 
+    parse_table[("B", "b")]             = ["F", "D", "E"] 
+    parse_table[("B", "c")]             = ["F", "D", "E"] 
+    parse_table[("B", "0")]             = ["error"] 
+    parse_table[("B", "1")]             = ["error"] 
+    parse_table[("B", "2")]             = ["error"] 
+    parse_table[("B", "true")]          = ["error"] 
+    parse_table[("B", "false")]         = ["error"] 
+    parse_table[("B", "fmt.Print(a)")]  = ["error"]
+    parse_table[("B", "a=a+b")]         = ["error"]
+    parse_table[("B", "EOS")]           = ["error"] 
+
+    parse_table[("C", "for")]           = ["error"]
+    parse_table[("C", ";")]             = ["error"] 
+    parse_table[("C", "=")]             = ["error"] 
+    parse_table[("C", "++")]            = ["++"] 
+    parse_table[("C", "--")]            = ["--"]
+    parse_table[("C", "{")]             = ["error"] 
+    parse_table[("C", "}")]             = ["error"] 
+    parse_table[("C", "==")]            = ["error"] 
+    parse_table[("C", "!=")]            = ["error"]  
+    parse_table[("C", ">")]             = ["error"] 
+    parse_table[("C", "<")]             = ["error"] 
+    parse_table[("C", ">=")]            = ["error"] 
+    parse_table[("C", "<=")]            = ["error"] 
+    parse_table[("C", "a")]             = ["error"] 
+    parse_table[("C", "b")]             = ["error"] 
+    parse_table[("C", "c")]             = ["error"] 
+    parse_table[("C", "0")]             = ["error"] 
+    parse_table[("C", "1")]             = ["error"] 
+    parse_table[("C", "2")]             = ["error"] 
+    parse_table[("C", "true")]          = ["error"] 
+    parse_table[("C", "false")]         = ["error"] 
+    parse_table[("C", "fmt.Print(a)")]  = ["error"]
+    parse_table[("C", "a=a+b")]         = ["error"]
+    parse_table[("C", "EOS")]           = ["error"] 
+
+    parse_table[("D", "for")]           = ["error"]
+    parse_table[("D", ";")]             = ["error"] 
+    parse_table[("D", "=")]             = ["error"] 
+    parse_table[("D", "++")]            = ["error"] 
+    parse_table[("D", "--")]            = ["error"]
+    parse_table[("D", "{")]             = ["error"] 
+    parse_table[("D", "}")]             = ["error"] 
+    parse_table[("D", "==")]            = ["=="] 
+    parse_table[("D", "!=")]            = ["!="]  
+    parse_table[("D", ">")]             = [">"] 
+    parse_table[("D", "<")]             = ["<"] 
+    parse_table[("D", ">=")]            = [">="] 
+    parse_table[("D", "<=")]            = ["<="] 
+    parse_table[("D", "a")]             = ["error"] 
+    parse_table[("D", "b")]             = ["error"] 
+    parse_table[("D", "c")]             = ["error"] 
+    parse_table[("D", "0")]             = ["error"] 
+    parse_table[("D", "1")]             = ["error"] 
+    parse_table[("D", "2")]             = ["error"] 
+    parse_table[("D", "true")]          = ["error"] 
+    parse_table[("D", "false")]         = ["error"] 
+    parse_table[("D", "fmt.Print(a)")]  = ["error"]
+    parse_table[("D", "a=a+b")]         = ["error"]
+    parse_table[("D", "EOS")]           = ["error"] 
+
+    parse_table[("E", "for")]           = ["error"]
+    parse_table[("E", ";")]             = ["error"] 
+    parse_table[("E", "=")]             = ["error"] 
+    parse_table[("E", "++")]            = ["error"] 
+    parse_table[("E", "--")]            = ["error"]
+    parse_table[("E", "{")]             = ["error"] 
+    parse_table[("E", "}")]             = ["error"] 
+    parse_table[("E", "==")]            = ["error"] 
+    parse_table[("E", "!=")]            = ["error"]  
+    parse_table[("E", ">")]             = ["error"] 
+    parse_table[("E", "<")]             = ["error"] 
+    parse_table[("E", ">=")]            = ["error"] 
+    parse_table[("E", "<=")]            = ["error"] 
+    parse_table[("E", "a")]             = ["a"] 
+    parse_table[("E", "b")]             = ["b"] 
+    parse_table[("E", "c")]             = ["c"] 
+    parse_table[("E", "0")]             = ["0"] 
+    parse_table[("E", "1")]             = ["1"] 
+    parse_table[("E", "2")]             = ["2"] 
+    parse_table[("E", "true")]          = ["true"] 
+    parse_table[("E", "false")]         = ["false"] 
+    parse_table[("E", "fmt.Print(a)")]  = ["error"]
+    parse_table[("E", "a=a+b")]         = ["error"]
+    parse_table[("E", "EOS")]           = ["error"] 
+
+    parse_table[("F", "for")]           = ["error"]
+    parse_table[("F", ";")]             = ["error"] 
+    parse_table[("F", "=")]             = ["error"] 
+    parse_table[("F", "++")]            = ["error"] 
+    parse_table[("F", "--")]            = ["error"]
+    parse_table[("F", "{")]             = ["error"] 
+    parse_table[("F", "}")]             = ["error"] 
+    parse_table[("F", "==")]            = ["error"] 
+    parse_table[("F", "!=")]            = ["error"]  
+    parse_table[("F", ">")]             = ["error"] 
+    parse_table[("F", "<")]             = ["error"] 
+    parse_table[("F", ">=")]            = ["error"] 
+    parse_table[("F", "<=")]            = ["error"] 
+    parse_table[("F", "a")]             = ["a"] 
+    parse_table[("F", "b")]             = ["b"] 
+    parse_table[("F", "c")]             = ["c"] 
+    parse_table[("F", "0")]             = ["error"] 
+    parse_table[("F", "1")]             = ["error"] 
+    parse_table[("F", "2")]             = ["error"] 
+    parse_table[("F", "true")]          = ["error"] 
+    parse_table[("F", "false")]         = ["error"] 
+    parse_table[("F", "fmt.Print(a)")]  = ["error"]
+    parse_table[("F", "a=a+b")]         = ["error"]
+    parse_table[("F", "EOS")]           = ["error"] 
+
+    parse_table[("G", "for")]           = ["error"]
+    parse_table[("G", ";")]             = ["error"] 
+    parse_table[("G", "=")]             = ["error"] 
+    parse_table[("G", "++")]            = ["error"] 
+    parse_table[("G", "--")]            = ["error"]
+    parse_table[("G", "{")]             = ["error"] 
+    parse_table[("G", "}")]             = ["error"] 
+    parse_table[("G", "==")]            = ["error"] 
+    parse_table[("G", "!=")]            = ["error"]  
+    parse_table[("G", ">")]             = ["error"] 
+    parse_table[("G", "<")]             = ["error"] 
+    parse_table[("G", ">=")]            = ["error"] 
+    parse_table[("G", "<=")]            = ["error"] 
+    parse_table[("G", "a")]             = ["error"] 
+    parse_table[("G", "b")]             = ["error"] 
+    parse_table[("G", "c")]             = ["error"] 
+    parse_table[("G", "0")]             = ["error"] 
+    parse_table[("G", "1")]             = ["error"] 
+    parse_table[("G", "2")]             = ["error"] 
+    parse_table[("G", "true")]          = ["error"] 
+    parse_table[("G", "false")]         = ["error"] 
+    parse_table[("G", "fmt.Print(a)")]  = ["fmt.Print(a)"]
+    parse_table[("G", "a=a+b")]         = ["a=a+b"]
+    parse_table[("G", "EOS")]           = ["error"] 
+
+    stack = []
+    stack.append('#')
+    stack.append('S')
+
+    index_token = 0
+    symbol = tokens[index_token]
+
+    while(len(stack) > 0):
+        top = stack[ len(stack) - 1 ]
+        print('Top    = ', top)
+        print('Symbol = ', symbol)
+        if top in terminals:
+            print('Top Adalah Simbol Terminal')
+            if top == symbol:
+                stack.pop()
+                index_token = index_token + 1
+                symbol = tokens[index_token]
+                if symbol == "EOS":
+                    stack.pop()
+                    print('Isi Stack:', stack)
             else:
-                if not self.parse_terminal(symbol):
-                    print("Susunan token tidak memenuhi aturan grammar")
-                    return
-        if self.current_token[0] != TOKEN_UNKNOWN:
-            print("Susunan token memenuhi aturan grammar")
+                print('ERROR')
+                break;
+        elif top in non_terminals:
+            print('Top Adalah Simbol Non-Terminal')
+            try:
+                if parse_table[(top, symbol)][0] != 'error':
+                    stack.pop()
+                    symbol_to_be_pushed = parse_table[(top, symbol)]
+                    for i in range(len(symbol_to_be_pushed)-1, -1, -1):
+                        stack.append(symbol_to_be_pushed[i])
+                else:
+                    print('ERROR')
+                    break;
+            except:
+                print('ERROR')
+                break;
         else:
-            print("Susunan token tidak memenuhi aturan grammar")
+            print('ERROR')
+            break;
+        print('Isi Stack: ', stack)
+        print()
 
-    def parse_terminal(self, terminal):
-        if self.current_token[0] == terminal:
-            self.advance()
-            return True
-        else:
-            return False
+    print()
+    if symbol == 'EOS' and len(stack) == 0:
+        print('Input Diterima')
+    else:
+        print('ERROR, Input Ditolak')  
+    
+    return parser
 
-    def advance(self):
-        self.current_token_index += 1
-        if self.current_token_index < len(self.tokens):
-            self.current_token = self.tokens[self.current_token_index]
-        else:
-            self.current_token = (None, None)
+#Main Program Parser
 
+"======= Contoh Input Diterima ======"
+"for a = 0 ; a < 2 ; a ++ { fmt.Print(a) }"
+"for a = 2 ; a >= 1 ; a -- { fmt.Print(a) }"
+"for a <= 2 { a=a+b }"
+"for a > 0 { fmt.Print(a) }"
 
-# Tokenize input source code
-def tokenize(source_code):
-    tokens = []
-    source_code = source_code.strip()
-    while source_code:
-        match = None
-        for token_type, pattern in TOKEN_PATTERNS:
-            regex = re.compile(pattern)
-            match = regex.match(source_code)
-            if match:
-                value = match.group(0)
-                if token_type != TOKEN_WHITESPACE and token_type != TOKEN_UNKNOWN:
-                    tokens.append((token_type, value))
-                source_code = source_code[match.end():]
-                break
-        if not match:
-            tokens.append((TOKEN_UNKNOWN, source_code[0]))
-            return tokens  # Token tidak diterima
-    return tokens
+"======= Contoh Input Ditolak ======"
+"for a = 0 ; a < 2 ; a ++ { fmt.print(a) }"
+"for a = 1 ; a >= 2 ; a -- fmt.Print(a)"
+"for { a=a+b }"
 
-
-# Token patterns
-TOKEN_PATTERNS = [
-    (TOKEN_FOR, r'for'),
-    (TOKEN_SEMICOLON, r';'),
-    (TOKEN_ASSIGNMENT, r':='),
-    (TOKEN_IDENTIFIER, r'[a-c]+'),
-    (TOKEN_COMPARE_OP, r'==|!=|>|<|>=|<='),
-    (TOKEN_ARITHMETIC_OP, r'\+|-|\*|/|%'),
-    (TOKEN_INCREMENT, r'\+\+'),
-    (TOKEN_DECREMENT, r'--'),
-    (TOKEN_OPEN_BRACE, r'{'),
-    (TOKEN_CLOSE_BRACE, r'}'),
-    (TOKEN_BOOLEAN, r'true|false'),
-    (TOKEN_DIGIT, r'[0-2]'),
-    (TOKEN_STRING, r'`[a-c0-2]*`'),
-    (TOKEN_WHITESPACE, r'\s+'),
-    (TOKEN_UNKNOWN, r'.')
-]
-
-# Example usage
-source_code = 'for a := 0; b < 2; b++ { a = a + 1 }'
-tokens = tokenize(source_code)
-parser = Parser(tokens)
-parser.parse()
+print("====== Parser For Loop Golang ======")
+sentence = "for a = 0 ; a < 2 ; a ++ { fmt.Print(a) }"
+input_string = sentence+'#'
+parser(sentence)
+print("====================================")
